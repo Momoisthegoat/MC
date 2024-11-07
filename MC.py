a131dc52a7c5f1dfecb0fe40a38c1d0fa7808479
Momoisthegoat <mohamedelgajiji81@gmail.com>
2024-11-07 12:58:03 -0800

    Add files via upload

---
+++ MC.py
@@ -0,0 +1,48 @@
+x=0
+print ("welcome to chapter 1 quiz")
+print ("quetion 1: who won the fortnite world cup")
+print (" 1: Clix")
+print (" 2: lacy")
+print (" 3: Bugha")
+print (" 4: Ron")
+y = int(input())
+if y==3:
+    print ("correct")
+    x=x+1
+else : 
+    print ("incorrect")
+print ("quetion 2 when does the item shop reset")
+print ("1: 4")
+print ("2: 5")
+print ("3: 6")
+print ("4:7")
+y = int(input())
+if y==2:
+    print ("correct")
+    x=x+1
+else :
+    print ("incorrect")
+print ("quetion 3 what year did fortnite start")
+print (" 1: 2014")
+print (" 2: 2015")
+print (" 3  2016")
+print (" 4  2017")
+y = int(input())
+if y==4:
+    print ("correct")
+    x=x+1
+else : 
+    print ("incorrect")
+print (" quetion 4: how much is 1000 vbucks in dollers")
+print (" 1: 8")
+print (" 2: 9")
+print (" 3  10")
+print (" 4  11")
+y = int(input())
+if y==3:
+    print ("correct")
+    x=x+1
+else : 
+    print ("incorrect")
+
+print ("Your final score is,",x, "/4")
